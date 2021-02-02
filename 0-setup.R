c3700f7b6bf8e9556fb843fe65a81dda9c704587
geneh0 <52678798+geneh0@users.noreply.github.com>
2021-02-01 21:18:17 -0800

    Create 0-setup.R

---
+++ 0-setup.R
@@ -0,0 +1,11 @@
+if(!require(rmarkdown)) install.packages("rmarkdown") # to use RMD
+if(!require(knitr)) install.packages("knitr") # to knit to PDF/Word
+if(!require(epiR)) install.packages("epiR") 
+if(!require(stats)) install.packages("stats")
+if(!require(tidyverse)) install.packages("tidyverse")
+if(!require(readxl)) install.packages("readxl")
+if(!require(epitools)) install.packages("epitools")
+if(!require(exact2x2)) install.packages("exact2x2")
+if(!require(DescTools)) install.packages("DescTools")
+if(!require(MESS)) install.packages("MESS")
+if(!require(pwr)) install.packages("pwr")
